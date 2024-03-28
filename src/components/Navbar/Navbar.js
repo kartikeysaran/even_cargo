1eb3ed0892aade999c75e82d3921f5e42b28616c
kartikeysaran <kartikeyeyantra@gmail.com>
2024-03-28 14:45:41 +0530

    Fixed Navbar , Added animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,6 +12,13 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div>
       <nav>
@@ -19,25 +26,25 @@ const Navbar = () => {
           <img className="logo" src={ECLogo} alt="Logo" />
         </div>
         <div className="right">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/service">Service</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/" onClick={scrollToTop}>Home</Link>
+          <Link to="/about" onClick={scrollToTop}>About</Link>
+          <Link to="/service" onClick={scrollToTop}>Service</Link>
+          <Link to="/blog" onClick={scrollToTop}>Blog</Link>
+          <Link to="/contact" onClick={scrollToTop}>Contact</Link>
         </div>
         <div className="menu-icon" onClick={toggleMenu}>
-        <IoMdMenu/>
+          <IoMdMenu />
         </div>
       </nav>
       {showMenu && (
         <div className="popup-menu">
           <div className="popup-menu-content">
             <IoMdClose onClick={toggleMenu} className='menu-icon'/>
-            <Link to="/" onClick={toggleMenu}>Home</Link>
-            <Link to="/about" onClick={toggleMenu}>About</Link>
-            <Link to="/service" onClick={toggleMenu}>Service</Link>
-            <Link to="/blog" onClick={toggleMenu}>Blog</Link>
-            <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+            <Link to="/" onClick={() => {toggleMenu(); scrollToTop();}}>Home</Link>
+            <Link to="/about" onClick={() => {toggleMenu(); scrollToTop();}}>About</Link>
+            <Link to="/service" onClick={() => {toggleMenu(); scrollToTop();}}>Service</Link>
+            <Link to="/blog" onClick={() => {toggleMenu(); scrollToTop();}}>Blog</Link>
+            <Link to="/contact" onClick={() => {toggleMenu(); scrollToTop();}}>Contact</Link>
           </div>
         </div>
       )}
