69976a238abdd27c4fb7da919d81284754c98517
kartikeysaran <kartikeyeyantra@gmail.com>
2024-03-29 18:00:02 +0530

    Aboutus page updated

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -13,13 +13,37 @@ import mubeena from '../../assets/team/mubeena.jpg'
 import satyajit from '../../assets/mentors/satyajit.png'
 import karen from '../../assets/mentors/karen.png'
 import sudarshan from '../../assets/mentors/sudarshan.png'
-
+import Important1 from '../../assets/important-1.jpeg';
+import Important2 from '../../assets/important-2.png';
+import Important3 from '../../assets/important-3.png' ;
+import { FaArrowCircleRight, FaArrowCircleLeft} from "react-icons/fa";
 
 const About = () => {
   const our_story_text = [
     "The seed of what Even Cargo is today started with many questions - what is women empowerment when the reality is starkly the opposite? Why are women still unable to access spaces and opportunities that are rightfully theirs? Is there a way to strengthen the possibilities of change by becoming a better ally? The story of Even Cargo is a story of overcoming hurdles at every junction. With the idea of bringing visible change, the team of volunteer individuals set out to different resource-poor communities across the city of Delhi in search of potential women delivery associates. The proposition was simple - get the opportunity to be trained and join a workforce that can assure the dignity of work and a assured income. The problems, however, were much more complicated...",
     "The seed of what Even Cargo is today started with many questions - what is women empowerment when the reality is starkly the opposite? Why are women still unable to access spaces and opportunities that are rightfully theirs? Is there a way to strengthen the possibilities of change by becoming a better ally? The story of Even Cargo is a story of overcoming hurdles at every junction. With the idea of bringing visible change, the team of volunteer individuals set out to different resource-poor communities across the city of Delhi in search of potential women delivery associates. The proposition was simple - get the opportunity to be trained and join a workforce that can assure the dignity of work and a assured income. The problems, however, were much more complicated. Women cannot become delivery partners! They won't be able to sustain in this industry as it is physically demanding. Our many stakeholders, from women and their family members and the logistics industry, had notions rooted in the patriarchal set-up of what women should and should not do. The visibility of women in the logistics space was zero, owing to the idea that this kind of work is not meant for women. These statements only made the team more determined to break that notion. And in 2016, with a small number of delivery associates, the process of change began. We saw changes in the confidence of our delivery associates, but also in the mindset of our logistics partners, who became willing to bring inclusivity to their systems. The last seven years have taught us important lessons of leadership, of solidarity and of the power of allyship. We started with a what if women became delivery associates? and we want to reach a stage where this is not even a question anymore. The smile on the face of our associates is why we do what we do! And we will not stop until we fulfill our vision of gender equity. -Team Even Cargo"
   ];
+  const slides = [
+    {
+      image:Important1
+    },
+    {
+      image:Important2
+    },
+    {
+      image:Important3
+    },
+  ];
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
 
   const journeyData = [
     {
@@ -115,11 +139,29 @@ const About = () => {
     setShowReadMore(false);
   };
 
+  
+
   return (
     <div className='about-container'>
-      <h1>Women Empowerment<br />by Women Employment</h1>
-      <h3>Willing to join us ?</h3>
+      <h1>Pitfalls of the status quo</h1>
       <div className='about-container-rect'></div>
+      <div className='home-why-is-it-important-container__container-image'>
+          <FaArrowCircleLeft onClick={prevSlide} style={{cursor:'pointer'}}/>
+          {slides.map((slide, index) => {
+            return (
+              <div
+                className={index === current ? 'slide active' : 'slide'}
+                key={index}
+              >
+                {index === current && (
+                  <img src={slide.image} alt='travelimg' className='home-why-is-it-important-container_image' />
+                )}
+              </div>
+            );
+          })}
+          <FaArrowCircleRight onClick={nextSlide} style={{cursor:'pointer'}}/>
+        </div>
+        <div className='spacer'/>
       <div className='about-container-our-story'>
       <svg xmlns="http://www.w3.org/2000/svg" width="1043" height="653" viewBox="0 0 1043 653" fill="none" className='about-container-pink-clouds'>
         <path d="M277.678 523.016C81.2145 386.838 -86.4457 221.232 49.8841 64.8471C186.214 -91.5377 777.269 68.9975 973.733 205.175C1170.2 341.353 897.671 401.606 761.341 557.991C625.011 714.376 474.141 659.194 277.678 523.016Z" fill="#FFF4F2"/>
@@ -153,8 +195,8 @@ const About = () => {
       </svg>
       <h2>Our Team</h2>
       <div className="team-container">
-        {teamMembers.slice(0, 4).map((member, index) => (
-          <div key={index} className="team-member-card">
+        {teamMembers.slice(0, 3).map((member, index) => (
+          <div key={index} className="team-member-card-2">
             <img src={member.img} alt={member.name} className="team-member-image" />
             <h4 className="team-member-name">{member.name}</h4>
             <p className="team-member-position">{member.position}</p>
@@ -162,7 +204,7 @@ const About = () => {
         ))}
       </div>
       <div className="team-container">
-        {teamMembers.slice(4, 6).map((member, index) => (
+        {teamMembers.slice(3, 6).map((member, index) => (
           <div key={index} className="team-member-card">
             <img src={member.img} alt={member.name} className="team-member-image" />
             <h4 className="team-member-name">{member.name}</h4>
